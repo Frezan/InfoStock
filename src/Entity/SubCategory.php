9b93a57897bd6a150512dee2a3b15c33d9e2b560
Frezan <49377394+Frezan@users.noreply.github.com>
2022-07-04 11:41:30 +0200

    Add class Entity

---
+++ src/Entity/SubCategory.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubCategoryRepository::class)]
+class SubCategory
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'subCategories')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $catId;
+
+    #[ORM\OneToMany(mappedBy: 'subCat', targetEntity: Product::class)]
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCatId(): ?Category
+    {
+        return $this->catId;
+    }
+
+    public function setCatId(?Category $catId): self
+    {
+        $this->catId = $catId;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setSubCat($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getSubCat() === $this) {
+                $product->setSubCat(null);
+            }
+        }
+
+        return $this;
+    }
+}
